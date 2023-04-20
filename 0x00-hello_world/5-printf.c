4043819355bb78b6143f793dc62c0b2a16395834
Nesmagalal <nesmagalal022@gmail.com>
2023-04-20 10:24:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
   *main - Entry point
-   * Return: Always 0 (Success)
-    */
+  *
+  *descrption :A C program that prints with printf function
+  *
+  * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
