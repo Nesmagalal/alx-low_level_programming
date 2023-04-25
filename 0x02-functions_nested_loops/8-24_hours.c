52389439343c37f9cde78be07c1c70cdce7489e6
Nesmagalal <nesmagalal022@gmail.com>
2023-04-25 15:33:59 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,7 +20,7 @@ void jack_bauer(void)
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
-					-putchar(c + 48);
+					_putchar(c + 48);
 					_putchar(d + 48);
 					_putchar('\n');
 				}
