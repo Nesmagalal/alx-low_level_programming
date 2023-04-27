0e5b51d9e6f63d422043b0c5945c6bc57db56d1e
Nesmagalal <nesmagalal022@gmail.com>
2023-04-27 15:16:18 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_most_numbers - print 0 - 9 apart
+  * Do not print 2 and 4
+  * Return: always 0
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
