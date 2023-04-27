8c9415c38101b0c62a745124f5cc7dc4e02da122
Nesmagalal <nesmagalal022@gmail.com>
2023-04-27 16:35:05 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_diagonal - print adiagonal  line
+  * @n: is the number of the times
+  * the character should be printed
+*/
+void print_diagonal(int n)
+{
+	int p, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (p = 1; p <= n; p++)
+		{
+			for (s = 1; s <= p; s++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+
+		}
+	}
+}
