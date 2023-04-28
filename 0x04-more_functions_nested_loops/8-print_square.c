99730b540b4a8908e0f4c95877f558cd70eb7773
Nesmagalal <nesmagalal022@gmail.com>
2023-04-27 17:16:39 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,9 +13,9 @@ void print_square(int size)
 	{
 		int row, colum;
 
-		for (row = 0; row <= size; row++)
+		for (row = 0; row < size; row++)
 		{
-			for (colum = 0; colum <= size; colum++)
+			for (colum = 0; colum < size; colum++)
 			_putchar('#');
 		}
 		_putchar('\n');
