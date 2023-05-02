616d4b9d68702741e85d6f1f10f22d5eff6d4f12
Nesmagalal <nesmagalal022@gmail.com>
2023-05-02 10:21:03 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,14 @@
   */
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
-	int i;
+	char temp;
+	int l, i;
 
-	while (s[counter] != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
+	for (l = 0; s[l] != '\0'; ++l)
+	for (i = 0; i < l / 2; i++)
 	{
-		counter--;
-		rev + s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
 	}
 }
