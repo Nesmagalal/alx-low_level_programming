f41aa0ec0fd623b9c6891361331cb70f4582590b
Nesmagalal <nesmagalal022@gmail.com>
2023-05-04 18:23:39 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,21 +3,21 @@
   * _strcmp - function should compare two string
   * @s1: string 1
   * @s2: string 2
-  * Return: always 0
+  * Return: s1[i] - s2[i]
   */
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
+	int i;
 
-	while (*s1)
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
+			return (s1[i] - s2[i]);
 		}
-		s1++;
-		s2++;
+		i++;
 	}
 	return (0);
 }
