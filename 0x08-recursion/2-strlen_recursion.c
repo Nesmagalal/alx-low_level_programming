4b2c4d848b031fab2a1d5922c6e8839c1f8f4780
Nesmagalal <nesmagalal022@gmail.com>
2023-05-10 11:37:27 -0700

    0x08. C - Recursion C Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,13 @@
   */
 int _strlen_recursion(char *s)
 {
-	int i;
 
-	if (*s > '\0')
+	int i = 0;
+
+	if (*s)
 	{
-		i += _strlen_recursion(s + 1) + 1;
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
 	return (i);
 }
