6502c23ef65a6af3c9ca6e2584a54b55c87c5dda
Nesmagalal <nesmagalal022@gmail.com>
2023-05-14 00:53:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - print the name of the program
+  * @argc: counter
+  * @argv: array
+  * Return: always 0
+  */
+int main(int argc, char argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
