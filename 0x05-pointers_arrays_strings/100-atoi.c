d738a93647b056900c7fd4b28c22a28391806f7d
Nesmagalal <nesmagalal022@gmail.com>
2023-05-15 03:03:44 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,16 +7,34 @@
 int _atoi(char *s)
 {
 	int n = 0;
-	int i = 1;
+	int i = 0;
+	int d = 0;
+	int f = 0;
+	int l = 0;
+	int b = 0;
 
-	do {
-		if (*s == '-')
-			i *= -1;
-		else if (*s >= '0' && *s <= '9')
-			n = (n * 10) + (*s - '0');
-		else if (n > 0)
-			break;
-	} while (*s++);
+	while (s[l] != '\0')
+		l++;
+	while (i < l && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
 
-	return (n * i);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			b = s[i] - '0';
+			if (d % 2)
+				b = -b;
+			n = n * 10 + b;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
