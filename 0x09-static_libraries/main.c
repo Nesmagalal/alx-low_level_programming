2506a64cbb30f447af93f5f1a606028619f51e3e
Nesmagalal <nesmagalal022@gmail.com>
2023-05-15 08:21:45 -0700

    liball.a

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("\"At the end of the day, my goal was to be the best
+	_puts("\"At the end of the day, my goal was to be the best
 		hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
