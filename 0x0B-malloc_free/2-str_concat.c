ea8442dc8b3a63a48ed137a75547c631e66de08e
Nesmagalal <nesmagalal022@gmail.com>
2023-05-16 03:54:54 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+  * _strlen - find length of a string
+  * @s: string
+  * Return: int
+  */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+/**
   * *str_concat - function that concatenates two strings.
   * @s1: string
   * @s2: string
