877cd3e960b31c58bc31445680a18f724a9b428b
Nesmagalal <nesmagalal022@gmail.com>
2023-05-22 10:33:32 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a dog struct
+  * @name: dog name
+  * @age: dog age
+  * @owner:dog owner
+  * description: struct dog 0. Poppy
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
